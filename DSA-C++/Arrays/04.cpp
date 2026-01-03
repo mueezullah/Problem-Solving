ebecc1876ec0ba0a6c39e8e67a9cdb9b4403ce6a
mueezullah <mueezullahip8@gmail.com>
2026-01-03 22:18:41 +0500

    Move Zeroes (3 solutions)

---
+++ DSA-C++/Arrays/04.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        // APPROACH 1 (Brute Force) worst solution
+        int n = nums.size();
+
+        // for(int i = 0; i < n; i++){
+        //     for(int j = i+1; j < n; j++){
+        //         if(nums[i] == 0){
+        //             swap(nums[i], nums[j]);
+        //         }
+        //     }
+        // }
+
+        // APPROACH 2 (Brute Force) slightly better
+        // for(int i = 0; i < n; i++){
+        //     if(nums[i] == 0){
+        //         for(int j = i+1; j < n; j++){
+        //             if(nums[j] != 0){
+        //                 swap(nums[i], nums[j]);
+        //                 break;
+        //             }
+        //         }
+        //     }
+        // }
+
+        int p = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[p]);
+                p++;
+            }
+        }
+    }
+};
