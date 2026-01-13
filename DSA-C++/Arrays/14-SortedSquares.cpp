d7b6c6099542b5137e0c3267ffa7e2571ab18908
mueezullah <mueezullahip8@gmail.com>
2026-01-13 20:26:20 +0500

    Squares of Sorted Array

--- DSA-C++/Arrays/14-SortedSquares.cpp
+++ DSA-C++/Arrays/14-SortedSquares.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i = 0; i < nums.size(); i++){
-            nums[i] = nums[i] * nums[i];
+        // APPROACH 1 (Squaring and Sorting)
+        // for(int i = 0; i < nums.size(); i++){
+        //     nums[i] = nums[i] * nums[i];
+        // }
+        // sort(nums.begin(), nums.end());
+        // return nums;
+
+        // APPROACH 2 (Two Pointer)
+        int n = nums.size();
+        int s = 0, e = n - 1;
+        vector<int> ans(n);
+
+        for(int i = n-1; i >= 0; i--){
+
+            if(abs(nums[s]) > abs(nums[e])){
+                ans[i] = nums[s] * nums[s];
+                s++;
+            } else {
+                ans[i] = nums[e] * nums[e];
+                e--;
+            }
         }
-        sort(nums.begin(), nums.end());
-        return nums;
+        return ans;
     }
 };
