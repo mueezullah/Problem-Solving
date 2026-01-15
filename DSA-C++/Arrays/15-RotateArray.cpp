ec231407443c3e367a210855df2ee9eedcfc8ac4
mueezullah <mueezullahip8@gmail.com>
2026-01-15 23:39:47 +0500

    Rotate Array

--- DSA-C++/Arrays/15-RotateArray.cpp
+++ DSA-C++/Arrays/15-RotateArray.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
+    
+    void reverseArray(vector<int>& arr, int start, int end){
+        while(start < end){
+            swap(arr[start++], arr[end--]);
+        }
+    }
+
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> temp(n);
+        // APPROACH 1
+        // vector<int> temp(n);
 
-        for(int i = 0; i < n; i++){
-            temp[(i+k) % n] = nums[i];
-        }
+        // for(int i = 0; i < n; i++){
+        //     temp[(i+k) % n] = nums[i];
+        // }
 
-        nums = temp;
+        // nums = temp;
+
+        // APPROACH 2
+        k = k % n;
+        
+        // func calls
+        reverseArray(nums, 0, n-1);   // Reverse entire array
+        reverseArray(nums, 0, k-1);   // Reverse first k elements
+        reverseArray(nums, k, n-1);   // Reverse remaining elements
+
     }
 };
