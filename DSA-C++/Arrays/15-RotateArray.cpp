c425b180642a2990322b7e112ba20e24b8e45bed
mueezullah <mueezullahip8@gmail.com>
2026-01-16 15:39:20 +0500

    Rotate Array (3 Approaches)

--- DSA-C++/Arrays/15-RotateArray.cpp
+++ DSA-C++/Arrays/15-RotateArray.cpp
@@ -9,7 +9,7 @@ public:
 
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        // APPROACH 1
+        // APPROACH 1 (Extra Space)
         // vector<int> temp(n);
 
         // for(int i = 0; i < n; i++){
@@ -18,13 +18,18 @@ public:
 
         // nums = temp;
 
-        // APPROACH 2
-        k = k % n;
+        // APPROACH 2 (Using Helper function)
+        // k = k % n;
         
         // func calls
-        reverseArray(nums, 0, n-1);   // Reverse entire array
-        reverseArray(nums, 0, k-1);   // Reverse first k elements
-        reverseArray(nums, k, n-1);   // Reverse remaining elements
+        // reverseArray(nums, 0, n-1);   // Reverse entire array
+        // reverseArray(nums, 0, k-1);   // Reverse first k elements
+        // reverseArray(nums, k, n-1);   // Reverse remaining elements
 
+        // APPROACH 3 (Using STL reverse())
+        k = k % n; // Ensure k is within the range of [0-n]
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
