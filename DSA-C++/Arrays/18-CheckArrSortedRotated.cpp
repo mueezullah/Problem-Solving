51d45729327bfcc82d3766c864fb2253edd02019
mueezullah <mueezullahip8@gmail.com>
2026-01-21 00:41:33 +0500

    Check if Array is Sorted and Rotated

---
+++ DSA-C++/Arrays/18-CheckArrSortedRotated.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        
+        for(int i = 1; i < n; i++){
+            if(nums[i] < nums[i-1]){
+                count++;
+            }
+            if(count > 1) return false;
+        }
+
+        if(count == 1){
+            if(nums[0] >= nums[n-1]){
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
