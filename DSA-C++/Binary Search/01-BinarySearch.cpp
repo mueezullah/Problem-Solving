e18d1076042f50273347a9b9b58da36667f5073d
mueezullah <mueezullahip8@gmail.com>
2026-02-09 21:09:43 +0500

    Binary Search

---
+++ DSA-C++/Binary Search/01-BinarySearch.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int start = 0, end = n - 1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(target > nums[mid]){
+                start = mid + 1;
+            }
+            else if(target < nums[mid]){
+                end = mid - 1;
+            }
+            else { // target == nums[mid]
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
