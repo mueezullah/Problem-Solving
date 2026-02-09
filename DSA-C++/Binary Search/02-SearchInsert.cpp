71a3fc5b5165d9ccda915ad8e295dfb624277b6f
mueezullah <mueezullahip8@gmail.com>
2026-02-10 01:02:05 +0500

    Search Insert Position

---
+++ DSA-C++/Binary Search/02-SearchInsert.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        // TC -> O(log n)
+        // SC -> O(1)
+        int n = nums.size();
+        int start = 0, end = n - 1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(target > nums[mid]){
+                start = mid + 1;
+            }
+            else if (target < nums[mid]) {
+                end = mid - 1;
+            }
+            else { // target == nums[mid]
+                return mid;
+            }
+        }
+        return start;
+    }
+};
