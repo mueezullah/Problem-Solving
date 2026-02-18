a2ec9198ba6ab05a08a142704efb941e43442bf6
mueezullah <mueezullahip8@gmail.com>
2026-02-18 21:34:52 +0500

    Find Peak Element

--- DSA-C++/Binary Search/08-FindPeakElement.cpp
+++ DSA-C++/Binary Search/08-FindPeakElement.cpp
@@ -2,18 +2,24 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
+
         int start = 0, end = n - 1;
-        
+
         while(start < end){
             int mid = start + (end - start) / 2;
 
-            if(nums[mid + 1] < nums[mid]){
+            if((mid == 0 || nums[mid] > nums[mid - 1]) && 
+                (mid == n-1 || nums[mid] > nums[mid + 1])){
+                return mid;
+            }
+
+            if(nums[mid] > nums[mid + 1]){
                 end = mid;
             }
             else {
                 start = mid + 1;
             }
         }
-        return end;
+        return -1;
     }
 };
