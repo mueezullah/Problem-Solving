5bd4f1f0d10704a17a293f3df4e4b5218a9951fe
mueezullah <mueezullahip8@gmail.com>
2026-06-05 23:30:35 +0500

    Linked List Cycle

---
+++ DSA-C++/Linked List/07-ListCycle.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // APPROACH 1 (Hashset)
+        // TC -> O(n)
+        // SC -> O(n)
+        // if(head == NULL) return false;
+
+        // ListNode* curr = head;
+        // unordered_set<ListNode*> visited;
+        // while(curr != NULL){
+
+        //     if(visited.find(curr) != visited.end()){
+        //         return true;
+        //     }
+
+        //     visited.insert(curr);
+        //     curr = curr->next;
+        // }
+
+        // return false;
+
+        // APPROACH 2 (Slow & Fast Pointers)
+        // TC -> O(n)
+        // SC -> O(1)
+
+        if(head == NULL || head->next == NULL) return false;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow == fast){
+                return true;;
+            }
+        }
+
+        return false;
+    }
+};
