87e03fda72cf94e7e3426dccdef7d6942dbaaf23
mueezullah <mueezullahip8@gmail.com>
2026-07-20 23:26:09 +0500

    Simplify Path

---
+++ DSA-C++/Stack & Queue/13-SimplifyPath.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        
+        string token = "";
+        
+        stack<string> st; 
+        stringstream ss(path);
+
+        while(getline(ss, token, '/')){
+
+            if(token == "" || token == "."){
+                continue;
+            }
+            
+            if(token != ".."){
+                st.push(token);
+            }
+            else if(!st.empty()) {
+                st.pop();
+            }
+        }
+
+        if(st.empty()){
+            return "/";
+        }
+
+        string ans = "";
+
+        while(!st.empty()){
+            ans = "/" + st.top() + ans;
+            st.pop();
+        }
+
+        return ans;
+    }
+};
