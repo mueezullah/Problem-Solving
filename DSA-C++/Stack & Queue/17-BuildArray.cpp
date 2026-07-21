afdf0bd9530e8cc63cf02d4b73303904e7adb4d2
mueezullah <mueezullahip8@gmail.com>
2026-07-21 23:55:35 +0500

    Build an Array with Stack Operation

---
+++ DSA-C++/Stack & Queue/17-BuildArray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        int stream = 1;
+        int i = 0;
+        vector<string> ans;
+
+        while(i < target.size() && stream <= n){
+
+            ans.push_back("Push");
+            if(target[i] == stream){
+                i++;
+            }
+            else {
+                ans.push_back("Pop");
+            }
+
+            stream++;
+        }
+
+        return ans;
+    }
+};
